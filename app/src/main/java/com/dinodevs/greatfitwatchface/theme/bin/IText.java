493568e842b2eae01aafbec418d7e3ebc82e1f98
Andrea Forlin <andrea.forlin@dataservices.it>
2020-02-25 08:57:23 +0100

    new: dev: IText

---
+++ app/src/main/java/com/dinodevs/greatfitwatchface/theme/bin/IText.java
@@ -0,0 +1,35 @@
+package com.dinodevs.greatfitwatchface.theme.bin;
+
+public interface IText {
+    Integer getTopLeftX();
+
+    void setTopLeftX(Integer topLeftX);
+
+    Integer getTopLeftY();
+
+    void setTopLeftY(Integer topLeftY);
+
+    Integer getBottomRightX();
+
+    void setBottomRightX(Integer bottomRightX);
+
+    Integer getBottomRightY();
+
+    void setBottomRightY(Integer bottomRightY);
+
+    String getAlignment();
+
+    void setAlignment(String alignment);
+
+    Integer getSpacing();
+
+    void setSpacing(Integer spacing);
+
+    Integer getImageIndex();
+
+    void setImageIndex(Integer imageIndex);
+
+    Integer getImagesCount();
+
+    void setImagesCount(Integer imagesCount);
+}
